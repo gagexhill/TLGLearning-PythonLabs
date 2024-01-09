600c7f5edc868a99f281314b70635132e3e8a4ec
gagexhill <gagehill87@gmail.com>
2024-01-09 18:26:33 +0000

    shebang user input challenge complete

---
+++ challenge.py
@@ -0,0 +1,9 @@
+def main():
+
+    user_name = input("What is your name?")
+
+    days_of_week = input("What day of the week is it?")
+
+    print("Hello, " + user_name + "! Happy " + days_of_week + "!")
+
+main()        
