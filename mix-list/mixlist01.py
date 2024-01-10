b2b9c8159e86d4f89c635b947266caba2bc42e1f
gagexhill <gagehill87@gmail.com>
2024-01-10 17:47:18 +0000

    lab 30 'working with lists' complete

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# display the first item in the list
+print(f"IP addresses: {iplist[3]}, {iplist[4]}")
