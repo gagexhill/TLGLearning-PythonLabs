f3f895472f9590e33e192bb5c6139eefccb211a2
gagexhill <gagehill87@gmail.com>
2024-01-10 17:18:25 +0000

    included 'genre' and 'actor' input() and lists, as well as print()

--- wednesdaychallenge.py
+++ wednesdaychallenge.py
@@ -14,6 +14,16 @@ def main():
 
     movie = [fav_movie]
 
-    print(f"Hello {name[0].capitalize()}! you're {str(age[0])} years old and your favorite movie is {movie[0].capitalize()} !")
+    print(f"Hello {name[0].capitalize()}! you're {str(age[0])} years old and your favorite movie is {movie[0].title()}!")
+
+    user_genre = input("What is the genre of your favorite movie? ")
+
+    user_actor = input("Who is your favorite star actor in " + movie[0].title() + "?")
+
+    genre = [user_genre]
+
+    actor = [user_actor]
+
+    print(f"Interesting! You like {genre[0].capitalize()} movies, that's so funny, so do I. Although, I'm not sure about your choice of favorite actor... {actor[0].title()}. To each their own I suppose! Haha!! Good luck sport.")
 
 main()    
