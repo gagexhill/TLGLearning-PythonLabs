a288fd4ad20c787d79d44926f37c10c52df0613b
Gage Hill <gage.hill@tlgcohort.com>
2024-01-19 14:42:40 -0700

    Added environment variable for API key management to enhance security

--- week1project/emojidecoder.py
+++ week1project/emojidecoder.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-""" The script is a Python program designed to decode emoji's using text entered from a user. This script primarily uses the 'Emoji" module, additional information. (https://carpedm20.github.io/emoji/docs/index.html).This script uses the 'Emoji-API', additional information (https://emoji-api.com/)"""
+"""The script is a Python program designed to decode emoji's using text entered from a user. This script primarily uses the 'Emoji" module, additional information. (https://carpedm20.github.io/emoji/docs/index.html).This script uses the 'Emoji-API', additional information (https://emoji-api.com/)"""
 
+import os
 import requests
 import emoji
 
 #function to 'get' emoji-api data from a single emoji character
 def get_emoji_info(emoji_char):
     #api gets additional emoji information from 'Emoji API'
-    api_key = "fc43f370cb3af1a083c6940a71598264233b6116"
+    api_key = os.getenv('EMOJI_API_KEY') #retrieves evironment variable
     api_endpoint = f"https://emoji-api.com/emojis?access_key={api_key}&search={emoji_char}"
     response = requests.get(api_endpoint)
     if response.status_code == 200: #successful request
@@ -55,7 +56,7 @@ def main():
         name = input("What is your name? ")
         user_correct = input(f"\nYou told me your name is {name.title()}.\n\nIs this correct? ")
         if user_correct.lower() in ["yes", "y"]:
-            print(f"\n\n😃 Awesome name!😃\n\nHi {name.title()}! My name is Zoop and I'm going to help you decode those tough millennial emojis.\n")
+            print(f"\n😃 Awesome name!😃\n\nHi {name.title()}! My name is Zoop and I'm going to help you decode those tough millennial emojis.\n")
             break
         elif user_correct.lower() in ["no", "n"]:
             print("\n🤔 Sorry about that! Let's try again.")
@@ -82,4 +83,4 @@ if __name__ == "__main__":
     main()
 
 #TASKS (stretch goals):
-    # flask: turn project into web app
+    # flask: 
